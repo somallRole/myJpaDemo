c7aaf61ac83ac630993e99f7d34d7a9a04b56c6c
somallRole <41855257+somallRole@users.noreply.github.com>
2021-05-17 10:30:25 +0800

    程序初始化展示所有spring容器的bean对象

--- mytest/myspringbootJpademo/src/main/java/com/demo/frankxu/MyDemoApplication.java
+++ mytest/myspringbootJpademo/src/main/java/com/demo/frankxu/MyDemoApplication.java
@@ -4,11 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.CommandLineRunner;
+import com.demo.frankxu.common.ApplicationContextUtil;
 
 @SpringBootApplication
 public class MyDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) extends ApplicationContextUtil implements CommandLineRunner{
         SpringApplication.run(MyDemoApplication.class, args);
+    }
+    
+    @Override
+    public void run(String... args) {
+
+        String[] beans = getApplicationContext().getBeanDefinitionNames();
+        
+        for (String bean : beans)
+        {
+            System.out.println(bean + " of Type :: " + getBeanByName(bean).getClass());
+        }
     }
 }
